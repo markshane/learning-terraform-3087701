8da372eb164b0f870d1af9239a09dbd73316f987
markshane <markshane58@gmail.com>
2024-07-18 17:01:11 -0400

    fix 4

--- main.tf
+++ main.tf
@@ -55,20 +55,14 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  target_groups = [
-    {
+  target_groups = {
+    instance = {
       name_prefix      = "blog"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port      = 80
-        }
-      }
     }
-  ]
+  }
 
   listeners = {
     http = {
