813769cf91f86b916571a5f2c19456e9cbadb9e9
markshane <markshane58@gmail.com>
2024-07-18 17:07:47 -0400

    fix 5

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ module "alb" {
   security_groups = [module.blog_sg.security_group_id]
 
   target_groups = {
-    instance = {
+    blog_instance = {
       name_prefix      = "blog"
       protocol         = "HTTP"
       port             = 80
