789eb4c121c69c01fd6b037658321f893e12a527
markshane <markshane58@gmail.com>
2024-07-22 10:39:19 -0400

    Revert "autoscaling"
    This reverts commit dd5dace810e973906fcc7a90d244601cd83d0421.

--- main.tf
+++ main.tf
@@ -34,24 +34,20 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "7.7.0"
-  
-  name = "blog"
-  min_size = 1
-  max_size = 2
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_groups     = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  image_id            = data.aws_ami.app_ami.id
-  instance_type       = var.instance_type
+  subnet_id = module.blog_vpc.public_subnets[0]
 
+  tags = {
+    Name = "Learning Terraform"
+  }
 }
 
-module "blog_alb" {
+module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
